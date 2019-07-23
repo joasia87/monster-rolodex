2656677a9b8d07b1612a4cce217694dad162b71c
Joanna Chądzyńska <joanna.chadzynska87@gmail.com>
2019-07-23 15:03:19 +0200

    Updates

---
+++ precache-manifest.19b09da13ce4ab2b5aeda80e9c79ef3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007c399349af88baa56b0274e1d5b102",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ca6545004c7519e4ae29",
+    "url": "/monster-rolodex/static/css/main.99b165af.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ca6545004c7519e4ae29",
+    "url": "/monster-rolodex/static/js/main.4d2fbb97.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
